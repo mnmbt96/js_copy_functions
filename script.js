0d67a3a2ab1b5229bccf378fb9d3c14fb9faf7bb
Manami <vaattii1996@gmail.com>
2024-01-28 20:16:21 -0800

    use innerText to get value

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 console.log("JS running");
 
 function copyToClipboard(element) {
-  const copyText = document.querySelector(`.${element}`).children;
-  let finalText = "";
+  // const copyText = document.querySelector(`.${element}`).children;
+  // let finalText = "";
 
-  for (let i = 0; i < copyText.length; i++) {
-    const child = copyText[i];
-    if (child.tagName === "DIV") {
-      finalText += child.textContent + "\n"; // テキストを取得して改行を追加
-    } else if (child.tagName === "BR") {
-      finalText += "\n"; // <br> タグがある場合は改行を追加
-    }
-  }
+  // for (let i = 0; i < copyText.length; i++) {
+  //   const child = copyText[i];
+  //   if (child.tagName === "DIV") {
+  //     finalText += child.textContent + "\n"; // テキストを取得して改行を追加
+  //   } else if (child.tagName === "BR") {
+  //     finalText += "\n"; // <br> タグがある場合は改行を追加
+  //   }
+  // }
 
-  console.log(finalText);
+  // console.log(finalText);
 
-  const textarea = $("<textarea>") // 新しい <textarea> 要素を作成
-    .appendTo("body") // 作成した <textarea> 要素を文書の <body> 要素に追加
-    .val(finalText) // <textarea> 要素の値（テキスト）を、変数copyTextの値に設定
-    .select(); // 要素内のテキストを選択状態にする
+  // const textarea = $("<textarea>") // 新しい <textarea> 要素を作成
+  //   .appendTo("body") // 作成した <textarea> 要素を文書の <body> 要素に追加
+  //   .val(str) // <textarea> 要素の値（テキスト）を、変数copyTextの値に設定
+  //   .select(); // 要素内のテキストを選択状態にする
+
+  const originalElement = document.querySelector(`.${element}`);
+  const str = originalElement.innerText; // または originalElement.textContent;
+
+  console.log(str);
+
+  const textarea = document.createElement("textarea"); // 新しい <textarea> 要素を作成
+  document.body.appendChild(textarea); // 作成した <textarea> 要素を文書の <body> 要素に追加
+  textarea.value = str; // <textarea> 要素の値（テキスト）を、変数 str の値に設定
+  textarea.select(); // 要素内のテキストを選択状態にする
 
   try {
     // クリップボードAPIを使用して選択されたテキストをクリップボードにコピー
-    navigator.clipboard.writeText(finalText);
+    navigator.clipboard.writeText(str);
     console.log("テキストがクリップボードにコピーされました！");
   } catch (err) {
     console.error("テキストをクリップボードにコピーできませんでした:", err);
